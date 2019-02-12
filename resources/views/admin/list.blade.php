18f984de17053143dca843b789826ada71c26fb3
diepzdeptraivodichthienha <shock88h@gmail.com>
2019-02-12 21:21:07 +0700

    view list

---
+++ resources/views/admin/list.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('title', 'list truyen')
+@section('content')
+
+
+ <div class="container">
+     <table class="table">
+         <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+            </tr>
+         </thead>
+         <tbody>
+            <tr>
+                <th scope="row">1</th>
+            </tr>
+
+            <tr>
+                <th scope="row">1</th>
+            </tr>
+
+            <tr>
+                <th scope="row">1</th>
+            </tr>
+         </tbody>
+     </table>
+ </div>
+
+
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,12 +69,17 @@
                             <li class="nav-item dropdown">
 
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    Danh sach truyen <span class="caret"></span>
+                                    Truyen <span class="caret"></span>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{route('admin.list')}}">
+                                        Danh sach truyen
+                                    </a>
+
                                     <a class="dropdown-item" href="{{route('admin.create')}}">
                                         Them moi truyen
                                     </a>
+
 
 
                                 </div>
